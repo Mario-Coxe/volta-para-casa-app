aa15e3114f428c4ea85847c4619a1c5753fd3875
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-05 00:57:50 +0100

    feat: Custom Bottom Tab Navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,162 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+/** @format */
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+import { View, Text } from "react-native";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function Layout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 27,
+          left: 16,
+          right: 16,
+          height: 72,
+          elevation: 0,
+          backgroundColor: "white",
+          borderRadius: 16,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                paddingTop: 10,
+              }}
+            >
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                color={focused ? "#F02A4B" : "gray"}
+                size={24}
+              />
+              <Text
+                style={{
+                  color: focused ? "#F02A4B" : "gray",
+                  fontSize: 12,
+                  marginTop: 4,
+                }}
+              >
+                Home
+              </Text>
+            </View>
           ),
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                paddingTop: 10,
+              }}
+            >
+              <Ionicons
+                name={focused ? "search" : "search-outline"}
+                color={focused ? "#F02A4B" : "gray"}
+                size={24}
+              />
+              <Text
+                style={{
+                  color: focused ? "#F02A4B" : "gray",
+                  fontSize: 12,
+                  marginTop: 4,
+                }}
+              >
+                Search
+              </Text>
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="add"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                height: 56,
+                width: 56,
+                borderRadius: 999,
+                backgroundColor: "#F02A4B",
+                marginBottom: 26
+              }}
+            >
+              <Ionicons name="add" color="white" size={24} />
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="likes"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                paddingTop: 10,
+              }}
+            >
+              <Ionicons
+                name={focused ? "heart" : "heart-outline"}
+                color={focused ? "#F02A4B" : "gray"}
+                size={24}
+              />
+              <Text
+                style={{
+                  color: focused ? "#F02A4B" : "gray",
+                  fontSize: 12,
+                  marginTop: 4,
+                }}
+              >
+                Likes
+              </Text>
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                paddingTop: 10,
+              }}
+            >
+              <Ionicons
+                name={focused ? "person" : "person-outline"}
+                color={focused ? "#F02A4B" : "gray"}
+                size={24}
+              />
+              <Text
+                style={{
+                  color: focused ? "#F02A4B" : "gray",
+                  fontSize: 12,
+                  marginTop: 4,
+                }}
+              >
+                Profile
+              </Text>
+            </View>
           ),
         }}
       />
---
+++ app/(tabs)/likes.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Likes = () => {
+  return (
+    <View>
+      <Text>likes</Text>
+    </View>
+  )
+}
+
+export default Likes
